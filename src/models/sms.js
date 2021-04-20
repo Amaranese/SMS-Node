d819b14dd208a0c285747a447976f8ec65fb5a75
Amaranese <alejandro.maranes@asendia.com>
2021-04-20 18:42:59 +0200

    .

---
+++ src/models/sms.js
@@ -0,0 +1,13 @@
+const { model, Schema} = require('mongoose');
+const newSchema = new Schema({
+  Body: {
+    type: String,
+    required: true
+  },
+  From: {
+    type: String
+  }
+}, {
+  timestamps: true
+});
+module.exports = model('sms', newSchema);
